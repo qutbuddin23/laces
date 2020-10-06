753261b99b0a978ff4eff08f3c26e4946ec7ef36
qutbuddin23 <qutbuddinunwala@gmail.com>
2020-10-06 21:48:21 +0530

    first commit

---
+++ common/menu.php
@@ -0,0 +1,29 @@
+<header id="header">
+						<div class="inner">
+
+							<!-- Logo -->
+								<a href="index.html" class="logo">
+									<span class="symbol"><img src="images/logo.svg" alt="" /></span><span class="title">Phantom</span>
+								</a>
+
+							<!-- Nav -->
+								<nav>
+									<ul>
+										<li><a href="#menu">Menu</a></li>
+									</ul>
+								</nav>
+
+						</div>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<h2>Menu</h2>
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="about.php">about</a></li>
+							<li><a href="galleys.php">gallery</a></li>
+							<li><a href="laces.php">types of laces</a></li>
+							<li><a href="contact.php">contatc</a></li>
+						</ul>
+					</nav>
---
+++ galleys.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<!--
+	Phantom by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<?php
+		include_once('common/header.php');
+	?>
+	<body class="is-preload">
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<?php
+						include_once('common/menu.php');
+					?>
+
+
+				<!-- Footer -->
+                <?php
+                    include_once('common/footer.php');
+                ?>	
+
+			</div>
